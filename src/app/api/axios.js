68ed131de899da23558f08c4929c5b4aadc4d993
JoseDEV799 <josedev799@gmail.com>
2024-10-07 01:54:28 -0500

    headers

--- src/app/api/axios.js
+++ src/app/api/axios.js
@@ -2,11 +2,10 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL,
-    // headers: {
-    //     'Content-Type': 'application/json',
-    //     'Accept': 'application/json',  // Agregado encabezado Accept
-    //     // Agrega otros encabezados aquí si es necesario
-    // },
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',  // Agregado encabezado Accept
+    },
     withCredentials: true,
 })
 
