eeea353b9f2b5748499bf1c53fcb9d648b9bc55a
JoseDEV799 <josedev799@gmail.com>
2024-10-07 01:37:11 -0500

    headers

--- src/app/api/axios.js
+++ src/app/api/axios.js
@@ -2,7 +2,11 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL,
-    withCredentials: true
+    headers: {
+        'Content-Type': 'application/json',
+        // Agrega otros encabezados aquí si es necesario
+    },
+    withCredentials: true,
 })
 
 export default instance
