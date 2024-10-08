61cd772452723bfd41405fb55ee74e610a5beb4b
JoseDEV799 <josedev799@gmail.com>
2024-10-08 10:39:01 -0500

    Restrict

--- src/app/api/book.js
+++ src/app/api/book.js
@@ -2,12 +2,12 @@ import axios from "./axios"
 
 export const allBooks = () => axios.get('books')
 
-export const bookCategory = (customParam) => axios.post('/books/category', customParam)
+export const bookCategory = (customParam) => axios.post('books/category', customParam)
 
-export const registerBook = book => axios.post('/books/store',book)
+export const registerBook = book => axios.post('books/store',book)
 
-export const updateBook = book => axios.post('/books/store',book)
+export const updateBook = book => axios.post('books/store',book)
 
-export const deleteBook = id => axios.post('/books/store',id)
+export const deleteBook = id => axios.post('books/store',id)
 
-export const searchBook = searchBookInput => axios.post('/books/search', searchBookInput)
+export const searchBook = searchBookInput => axios.post('books/search', searchBookInput)
--- src/app/api/category.js
+++ src/app/api/category.js
@@ -1,3 +1,3 @@
 import axios from "./axios"
 
-export const AllCategories = () => axios.get('/category')
+export const AllCategories = () => axios.get('category')
