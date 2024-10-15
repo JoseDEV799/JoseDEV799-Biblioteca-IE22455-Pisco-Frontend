e66a6f0d8dd919aa8b3878403af090e7e198a7cf
JoseDEV799 <josedev799@gmail.com>
2024-10-14 21:04:23 -0500

    Domain

--- src/app/api/auth.js
+++ src/app/api/auth.js
@@ -2,7 +2,7 @@ import axios from './axios'
 
 export const registerUser = user => axios.post('register', user)
 
-export const loginRequest = user => axios.post('login', user)
+export const loginRequest = user => axios.post('/login', user)
 
 export const logoutRequest = token => axios.post('logout', token)
 
